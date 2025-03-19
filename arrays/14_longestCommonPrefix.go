8b325b45500fa8aad253ab87166605e09f468be4
L1z1ng3r-sswe <krasavachic@gmail.com>
2025-03-19 13:16:45 +0600

    feat: 198_house_robber done

--- arrays/14_longestCommonPrefix.go
+++ arrays/14_longestCommonPrefix.go
@@ -1,34 +1,32 @@
-func longestCommonPrefix(strs []string) string {
-	var res string
-	minLen := smallestStr(strs)
-
-	for i := range minLen {
-		currSymbol := strs[0][i]
-
-		for _, word := range strs {
-			if word[i] != currSymbol {
-				return res
-			}
-		}
-		res += string(currSymbol)
+func rob(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
 	}
 
-	return res
-}
+	maxVal := max(nums[0], nums[1])
 
-func smallestStr(strs []string) int {
-	res := math.MaxInt64
-
-	for _, word := range strs {
-		res = min(res, len(word))
+	if len(nums) == 2 {
+		return maxVal
 	}
 
-	return res
+	prevMaxVal := nums[0]
+
+	for i := 2; i < len(nums); i++ {
+		newMaxVal := max(prevMaxVal+nums[i], maxVal)
+		prevMaxVal = maxVal
+		maxVal = newMaxVal
+	}
+
+	return maxVal
 }
 
-func min(a, b int) int {
-	if a < b {
+func max(a, b int) int {
+	if a > b {
 		return a
 	}
+
 	return b
 }
+
+// time: O(n)
+// space: O(n)
---
+++ arrays/198_house_robber.go
@@ -0,0 +1,29 @@
+func rob(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	maxVal := max(nums[0], nums[1])
+
+	if len(nums) == 2 {
+		return maxVal
+	}
+
+	prevMaxVal := nums[0]
+
+	for i := 2; i < len(nums); i++ {
+		newMaxVal := max(prevMaxVal+nums[i], maxVal)
+		prevMaxVal = maxVal
+		maxVal = newMaxVal
+	}
+
+	return maxVal
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
