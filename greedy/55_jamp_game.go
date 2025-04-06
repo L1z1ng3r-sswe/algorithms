db92ed389f217f50b00e7fc9cd7b01b7cc22cbb7
L1z1ng3r-sswe <krasavachic@gmail.com>
2025-04-06 19:42:13 +0600

    feat: 55_jamp_game

---
+++ greedy/55_jamp_game.go
@@ -0,0 +1,46 @@
+func canJump(nums []int) bool {
+	target := len(nums) - 1
+
+	dp := make(map[int]struct{}, len(nums))
+
+	var dfs func(idx int) bool
+	dfs = func(idx int) bool {
+		if idx >= target {
+			return true
+		}
+
+		for i := nums[idx]; i > 0; i-- {
+			if _, ok := dp[idx+i]; !ok && dfs(idx+i) {
+				return true
+			}
+		}
+
+		dp[idx] = struct{}{}
+		return false
+	}
+
+	return dfs(0)
+}
+
+// time: O(n^2)
+// space: O(n)
+
+// greedy
+
+func canJump(nums []int) bool {
+	n := len(nums)
+	target := n - 1
+
+	for i := n - 2; i >= 0; i-- {
+		num := nums[i]
+
+		if i+num >= target {
+			target = i
+		}
+	}
+
+	return target == 0
+}
+
+// time: O(n)
+// space: O(n)
