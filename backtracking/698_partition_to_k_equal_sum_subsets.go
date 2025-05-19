9237fec7f40bf12648a865f668427c15dbd5656e
L1z1ng3r-sswe <krasavachic@gmail.com>
2025-05-19 16:09:15 +0600

    feat: 698_parition_to_k_equal_sum_subsets 2 approaches: buckets and groups

--- backtracking/698_partition_to_k_equal_sum_subsets.go
+++ backtracking/698_partition_to_k_equal_sum_subsets.go
@@ -50,3 +50,62 @@ func canPartitionKSubsets(nums []int, k int) bool {
 
 // time: O(k ^ n)
 // space (k + n) k for buckets, n for max recursion depth
+
+func canPartitionKSubsets(nums []int, k int) bool {
+	var totalSum int
+	for _, num := range nums {
+		totalSum += num
+	}
+	if totalSum%k != 0 {
+		return false
+	}
+	target := totalSum / k
+
+	sort.Sort(sort.Reverse(sort.IntSlice(nums))) // Descending order optimization
+	used := make([]bool, len(nums))
+
+	var backtrack func(idx int, currSum int, kLeft int) bool
+	backtrack = func(idx int, currSum int, kLeft int) bool {
+		// basecase
+		if kLeft == 1 {
+			return true
+		}
+
+		// if the current group is filled - start filling the next group
+		if currSum == target {
+			return backtrack(0, 0, kLeft-1)
+		}
+
+		if idx >= len(nums) || currSum > target {
+			return false
+		}
+
+		for i := idx; i < len(nums); i++ {
+			if !used[i] {
+				used[i] = true
+				if backtrack(i+1, currSum+nums[i], kLeft) {
+					return true
+				}
+				used[i] = false
+
+				// optimization: the current number can not be placed anywhere - stop recursion
+				if currSum == 0 {
+					break
+				}
+
+				// optimization: skip duplicate numbers to avoid redundant work
+				for i < len(nums)-1 && nums[i] == nums[i+1] {
+					i++
+				}
+			}
+		}
+
+		return false
+	}
+
+	return backtrack(0, 0, k)
+}
+
+// time: O(k(2^n))
+// space: O(n)
+
